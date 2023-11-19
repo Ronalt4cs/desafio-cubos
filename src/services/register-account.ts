aeb7525dc0d45b8c5e0e64c1605f93d7dc304ae2
ronalt4cs <ronaltaugustocs@gmail.com>
2023-11-19 16:20:12 -0300

    feat: not allow register account duplicated

--- src/services/register-account.ts
+++ src/services/register-account.ts
@@ -1,11 +1,13 @@
 import { AccountsRepository } from '@/repositories/accounts-repository'
 import { InvalidateBranchError } from './errors/invalidate-branch-error'
 import { InvalidateAccountNumberError } from './errors/invalidate-account-number-error'
+import { ResourceNotFound } from './errors/resource-not-found'
+import { AccountAlreadyExistsError } from './errors/account-already-exists-error'
 
 interface RegisterAccountServiceRequest {
   branch: string
   account: string
-  userId: string
+  userId?: string
 }
 
 export class RegisterAccountService {
@@ -16,6 +18,9 @@ export class RegisterAccountService {
     account,
     userId
   }: RegisterAccountServiceRequest) {
+    if (!userId) {
+      throw new ResourceNotFound()
+    }
 
     if (branch.trim().length !== 3) {
       throw new InvalidateBranchError()
@@ -26,6 +31,12 @@ export class RegisterAccountService {
     }
 
     const accountFormated = account.replace(/\D/g, '').replace(/(\d{7})(\d+)/, '$1-$2')
+
+    const accountAlreadyExists = await this.accountsRepository.getByAccountNumber(accountFormated)
+
+    if (accountAlreadyExists) {
+      throw new AccountAlreadyExistsError()
+    }
 
     const accountCreated = await this.accountsRepository.create({
       branch: branch.trim(),
